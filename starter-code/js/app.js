9ced8ca76c2c1ca7026f967df4e740f8bc25181d
James Summers <jamesnsummers@gmail.com>
2017-04-27 16:16:24 -0500

    Practice making site responsive

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
 
 });
+
+$(function(){
+  $('header nav > ul > li:nth-child(4) ul li:nth-child(1)').on('click', function(){
+    event.preventDefault();
+    $(this).siblings().slideToggle();
+  });
+})
